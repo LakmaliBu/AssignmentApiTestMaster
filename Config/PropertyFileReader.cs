c11025b0ecc719eef82c13b1e53e54b4eddfe3fd
Lakmali Bulathsinghala <lakmalibu@gmail.com>
2024-10-08 23:24:05 +0530

    Add project files.

---
+++ Config/PropertyFileReader.cs
@@ -0,0 +1,30 @@
+﻿using AssignmentApiTestMaster.Utilities;
+using Authlete.Util;
+
+
+namespace AssignmentApiTestMaster.Config
+{
+    public class PropertyFileReader
+    {
+        public static IDictionary<string, string> properties;
+
+        public void Read()
+        {
+            string file = "config.properties";
+            try
+            {
+                using (TextReader reader = new StreamReader(file))
+                {
+                    properties = PropertiesLoader.Load(reader);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("Error while reading property file" + ex);
+
+
+            }
+        }
+    }
+}
---
+++ Utilities/Logger.cs
@@ -0,0 +1,22 @@
+﻿using AssignmentApiTestMaster.Config;
+using log4net;
+
+
+namespace AssignmentApiTestMaster.Utilities
+{
+    public static class Logger
+    {
+        static readonly ILog _logger = LogManager.GetLogger(typeof(PropertyFileReader));
+        public static void LogError(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public static void LogInfo(string message)
+        {
+            _logger.Info(message);
+
+        }
+    }
+
+}
